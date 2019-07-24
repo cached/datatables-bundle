365eb700c79fddc4449826aaf75db63663d0bd90
Michał Młodziński <michal.mlodzinski@isobar.com>
2019-07-24 13:56:38 +0200

    WIP: Column filter

---
+++ src/Resources/public/js/datatables-column-filter.js
@@ -0,0 +1,28 @@
+/**
+ * @param {*} uri 
+ * @param {*} parameter 
+ * @param {*} value 
+ */
+function addOrUpdateUriParameter(uri, parameter, value) {
+    var new_url = normalizeAmpersand(uri);
+    
+    new_url = URI(new_url).normalizeQuery();
+
+    if (new_url.hasQuery(parameter)) {
+        new_url.removeQuery(parameter);
+    }
+
+    if (value != '') {
+        new_url = new_url.addQuery(parameter, value);
+    }
+
+    return new_url.toString();
+}
+
+/**
+ * 
+ * @param {*} string 
+ */
+function normalizeAmpersand(string) {
+    return string.replace(/&amp;/g, "&").replace(/amp%3B/g, "");
+}
