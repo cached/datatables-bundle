0cb9ae0d38a5333e517907574a187f73befc152a
Michał Młodziński <mlodzinski+github@gmail.com>
2021-03-28 22:20:30 +0200

    Update datatables-column-filter.js

--- src/Resources/public/js/datatables-column-filter.js
+++ src/Resources/public/js/datatables-column-filter.js
@@ -1,7 +1,9 @@
 /**
- * @param {*} uri 
- * @param {*} parameter 
- * @param {*} value 
+ * @param uri
+ * @param parameter
+ * @param value
+ * @param isMultiple
+ * @returns {string}
  */
 function addOrUpdateUriParameter(uri, parameter, value, isMultiple) {
     var isMultipleQuery = isMultiple || false;
@@ -11,10 +13,36 @@ function addOrUpdateUriParameter(uri, parameter, value, isMultiple) {
 
     if (!isMultipleQuery && new_url.hasQuery(parameter)) {
         new_url.removeQuery(parameter);
+    } else if (isMultipleQuery && new_url.hasQuery(parameter)) {
+        new_url.removeQuery(parameter, value);
     }
+
 
     if (value != '') {
         new_url = new_url.addQuery(parameter, value);
+    }
+
+    return new_url.toString();
+}
+
+/**
+ * @param uri
+ * @param parameter
+ * @param value
+ * @param isMultiple
+ * @returns {string}
+ */
+function removeUriParameter(uri, parameter, value, isMultiple)
+{
+    var isMultipleQuery = isMultiple || false;
+    var new_url = normalizeAmpersand(uri);
+
+    new_url = URI(new_url).normalizeQuery();
+
+    if (!isMultipleQuery && new_url.hasQuery(parameter)) {
+        new_url.removeQuery(parameter);
+    } else if (isMultipleQuery && new_url.hasQuery(parameter)) {
+        new_url.removeQuery(parameter, value);
     }
 
     return new_url.toString();
